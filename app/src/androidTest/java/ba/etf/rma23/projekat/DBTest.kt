9fb3ea1e7cbb4c89a01c322fef9185765f665dba
ebabic1 <ebabic1@etf.unsa.ba>
2023-06-27 15:11:33 +0200

    stavljen moj hash i drugi id igre u test

--- app/src/androidTest/java/ba/etf/rma23/projekat/DBTest.kt
+++ app/src/androidTest/java/ba/etf/rma23/projekat/DBTest.kt
@@ -39,8 +39,8 @@ class DBTest {
 
     //pri testiranju zamjenite hash sa vašim i id igre umjesto 22 postavite na neki drugi broj
     // da ne bi došlo do konfuzije oko rezultata testova ako dva studenta istovremeno testiraju svoj kod
-    private val HASH = "ca0ee672-440b-45b2-8a12-75b80f4fbdd3"
-    private val idIGRE = 22
+    private val HASH = "c1641d62-60c3-4697-b9c0-f4791d204ffe"
+    private val idIGRE = 32
 
 
     private val countNotOnline =
